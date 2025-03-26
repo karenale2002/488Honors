3cc3981761cb288a248fe5e354fdc9f16d6924f8
Karen Olivares <156404434+karenale2002@users.noreply.github.com>
2025-03-26 17:41:05 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Cami el Camarón Asesor`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `“El camarón que se mueve, ¡gana clientes!”`;
+export const FOOTER_MESSAGE: string = `El camarón que se mueve, ¡gana clientes!`;
 export const CLEAR_BUTTON_TEXT: string = `Borrar chat`;
 export const PAGE_TITLE: string = `Camarón Asesor`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;

c76c79957af1c618c1577ee80da4b655e458d40c
Karen Olivares <156404434+karenale2002@users.noreply.github.com>
2025-04-10 13:30:35 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hola, soy ${AI_NAME}, el asistente de inteligencia artificial de ${OWNER_NAME}. Estoy aquí para ayudar a generar contenido de texto para publicar en las redes sociales para promover los servicios de American Tax Professional.`;
+export const INITIAL_MESSAGE: string = `Hola, soy ${AI_NAME}, el asistente de inteligencia artificial de ${OWNER_NAME}. Estoy aquí para ayudar a generar contenido de texto para publicar en las redes sociales para promover los servicios de American Tax Professionals.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Lo siento, tengo problemas para generar una respuesta. Inténtalo de nuevo más tarde.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;

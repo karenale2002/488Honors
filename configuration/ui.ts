20b6e353de6f13aa8ad9ef975b7f0a76f13aede0
Karen Olivares <156404434+karenale2002@users.noreply.github.com>
2025-04-10 13:34:19 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `El camarón que se mueve, ¡gana clientes!`;
 export const CLEAR_BUTTON_TEXT: string = `Borrar chat`;
 export const PAGE_TITLE: string = `Camarón Asesor`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Charla con  ${AI_NAME}, el asistente de IA de ${OWNER_NAME}.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
